066e114c803daa8245b66f4525c0af684862f927
David Carvalho <davidxp12@gmail.com>
2013-10-07 16:19:45 -0300

    Correção BUG.

--- src/command/CommandCadastrarPessoaFisica.java
+++ src/command/CommandCadastrarPessoaFisica.java
@@ -1,15 +1,30 @@
 package command;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
  
 import model.PessoaFisica;;
 
 public  class CommandCadastrarPessoaFisica extends Command {
 
 	 
-	public String executar(HttpServletRequest request,HttpServletResponse response) throws Exception {
+	/*public String executar(HttpServletRequest request,HttpServletResponse response) throws Exception {
 		 
+		PessoaFisica PessoaFisica = new PessoaFisica();
 		
+		
+		//novo.setChassi(request.getParameter("txtChassi"));
+		//PessoaFisica
+		
+		
+		return "teste";
+	}
+
+ */
+	@Override
+	public String executar(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
