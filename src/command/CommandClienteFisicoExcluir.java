becb92d9c4d2b29ad4076056d337f8aeb808478f
David Carvalho <davidxp12@gmail.com>
2013-10-10 11:17:24 -0300

    Excluir listar.

--- src/command/CommandClienteFisicoExcluir.java
+++ src/command/CommandClienteFisicoExcluir.java
@@ -1,5 +1,8 @@
 package command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,14 +10,22 @@ import model.PessoaFisica;
 
 public class CommandClienteFisicoExcluir extends Command {
 
-	@Override
+	 
 	public String executar(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 	
 		PessoaFisica clienteFisico = new PessoaFisica();
 		clienteFisico.setId(Integer.parseInt(request.getParameter("item")));
-		
 		clienteFisico.Excluir(clienteFisico.getId());
+		
+		PessoaFisica pf = new PessoaFisica();
+
+		List<PessoaFisica> retorno2 = new ArrayList<PessoaFisica>();
+
+		retorno2 = pf.obterClientes("", "");
+		request.getSession().setAttribute("listaClientesFisicos",
+				retorno2);
+		
 		
 		
 		return "frmClienteFisicoLista.jsp";
